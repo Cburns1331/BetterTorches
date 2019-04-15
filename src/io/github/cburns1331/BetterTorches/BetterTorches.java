3e7b9fdc0d605ba30f61c5a3272cd9bc577458a5
Chris Burns <burns.christopher13@gmail.com>
2019-04-14 22:03:07 -0400

    Blank plugin

---
+++ src/io/github/cburns1331/BetterTorches/BetterTorches.java
@@ -0,0 +1,16 @@
+package io.github.cburns1331.bettertorches;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterTorches extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
